b4d8d1724e750a9cc34861287f2154a75958296a
balogun14 <awwalbalogun06@gmail.com>
2023-07-20 20:27:48 +0100

    completed

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
-// Play & pause video
+//function🧷
 function toggleVideoStatus() {
   if (video.paused) {
     video.play();
@@ -12,8 +12,7 @@ function toggleVideoStatus() {
     video.pause();
   }
 }
-
-// update play/pause icon
+//update the icon
 function updatePlayIcon() {
   if (video.paused) {
     play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
@@ -21,45 +20,36 @@ function updatePlayIcon() {
     play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
-
-// Update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
-
-  // Get minutes
+  //🎶
   let mins = Math.floor(video.currentTime / 60);
   if (mins < 10) {
-    mins = '0' + String(mins);
+    mins = "0" + String(mins);
   }
-
-  // Get seconds
   let secs = Math.floor(video.currentTime % 60);
   if (secs < 10) {
-    secs = '0' + String(secs);
+    secs = "0" + String(secs);
   }
-
   timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-// Set video time to progress
 function setVideoProgress() {
   video.currentTime = (+progress.value * video.duration) / 100;
 }
-
-// Stop video
+//stop videos
 function stopVideo() {
   video.currentTime = 0;
   video.pause();
 }
+//EventListeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
 
-// Event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdate', updateProgress);
+play.addEventListener("click", toggleVideoStatus);
 
-play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
 
-stop.addEventListener('click', stopVideo);
-
-progress.addEventListener('change', setVideoProgress);
+progress.addEventListener("change", setVideoProgress);
